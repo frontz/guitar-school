c42a0d4f16d477147250f4abefad4280ba88d887
Tomasz Fraczek <tom.frontz@gmail.com>
2024-02-21 08:19:47 +0100

    bascic folders structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthFormComponent } from './auth/auth-form/auth-form.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthFormComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
