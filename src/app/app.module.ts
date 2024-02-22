d323befd3cbf2848177007b179895b6a0977e12c
Tomasz Fraczek <tom.frontz@gmail.com>
2024-02-22 10:36:47 +0100

    Application skeleton prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import {AuthModule} from "./auth/auth.module";
+import { AboutComponent } from './about/about.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
+import { UserViewComponent } from './user/user-view/user-view.component';
+import { BasketComponent } from './shop/basket/basket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    AboutComponent,
+    CoursesListComponent,
+    CourseDetailsComponent,
+    UserViewComponent,
+    BasketComponent,
   ],
   imports: [
     BrowserModule,
